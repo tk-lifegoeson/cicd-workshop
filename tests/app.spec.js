34fb39c5fdb9bf35490beb1f13dce5974a383262
Titaya Kumpoopong <titaya.kumpoopong@kyndryl.com>
2023-05-26 12:17:44 +0700

    Added my project

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -4,7 +4,13 @@ const app = require('../app');
 describe('GET /healthcheck', () => {
   it('responds with status: UP', async () => {
     const response = await request(app).get('/healthcheck');
-    expect(response.statusCode).toBe(400);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ status: 'UP' });
+  });
+
+  it('responds with status: UP', async () => {
+    const response = await request(app).get('/getpodinfo');
+    expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({ status: 'UP' });
   });
 });
